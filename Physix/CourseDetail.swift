7383e149895b97e22991cc916e5066edb3f52da4
OrestisVaggelis <orestis22000@gmail.com>
2022-05-26 18:18:18 +0300

    Update CourseDetail.swift

--- Physix/CourseDetail.swift
+++ Physix/CourseDetail.swift
@@ -1,11 +1,3 @@
-//
-//  CourseDetail.swift
-//  newStart
-//
-//  Created by Orestis Vaggelis on 28/8/20.
-//  Copyright © 2020 Orestis Vaggelis. All rights reserved.
-//
-
 import SwiftUI
 import SDWebImageSwiftUI
 
