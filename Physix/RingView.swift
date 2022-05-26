bb6d6282edb07831fb585f808f1844fedb8a6474
OrestisVaggelis <orestis22000@gmail.com>
2022-05-26 18:21:25 +0300

    Update RingView.swift

--- Physix/RingView.swift
+++ Physix/RingView.swift
@@ -1,11 +1,3 @@
-//
-//  RingView.swift
-//  newStart
-//
-//  Created by Orestis Vaggelis on 17/8/20.
-//  Copyright © 2020 Orestis Vaggelis. All rights reserved.
-//
-
 import SwiftUI
 
 struct RingView: View {
