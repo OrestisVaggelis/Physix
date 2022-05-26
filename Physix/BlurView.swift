5b988ef4e4b02c5e4f92c6658e713d8a4365f2dc
OrestisVaggelis <orestis22000@gmail.com>
2022-05-26 18:17:36 +0300

    Update BlurView.swift

--- Physix/BlurView.swift
+++ Physix/BlurView.swift
@@ -1,11 +1,3 @@
-//
-//  BlurView.swift
-//  newStart
-//
-//  Created by Orestis Vaggelis on 29/8/20.
-//  Copyright © 2020 Orestis Vaggelis. All rights reserved.
-//
-
 import SwiftUI
 
 struct BlurView: UIViewRepresentable {
